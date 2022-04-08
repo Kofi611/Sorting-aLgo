e41d734f7b615f83c3adb08bb9b988283343d563
Kofi611 <34664721+Kofi611@users.noreply.github.com>
2022-04-08 12:55:50 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+	char * inpstr = argv[1];
+	char * sorted = (char *) malloc(sizeof(char) * sizeof(argv[1]));
+	char * temp = ;
+	while(inpstr){
+		
+	}
+	return 0;
+}
